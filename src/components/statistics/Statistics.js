b4f23b1c3c73fbd2b63c797a78ba28b3fc766852
Tetiana Yarova <andronikova11@gmail.com>
2021-07-26 11:13:42 +0300

    Добавляет исправления/замечания ментора

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -5,9 +5,9 @@ import s from './Statistics.module.css';
 const Statistics = ({ title, stats }) => {
   return (
     <section className={s.statistics}>
-      {title ? <h2 className={s.title}>{title}</h2> : ''}
+      {title && <h2 className={s.title}>{title}</h2>}
 
-      <ul className={s.stat_list}>
+      <ul className={s.statList}>
         {stats.map(stat => (
           <li className={s.item} key={stat.id}>
             <span className={s.label}>{stat.label} </span>
@@ -17,6 +17,10 @@ const Statistics = ({ title, stats }) => {
       </ul>
     </section>
   );
+};
+
+Statistics.defaultProps = {
+  title: '',
 };
 
 Statistics.propTypes = {
